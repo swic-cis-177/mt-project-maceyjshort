d869351d7b0c1a37bde05c8c70768b1eddf169f1
Macey Short <maceyshort@student.swic.edu>
2020-10-10 00:02:57 -0700

    grab the button from html

---
+++ scripts.js
@@ -0,0 +1,7 @@
+//todo: Grab the "button" from html
+const button = document.querySelector("button");
+console.log(button);
+
+//TODO: Listen for a "click" on the 'button'
+
+//TODO: When there is a click, change "No information sent" to "Information sent"
