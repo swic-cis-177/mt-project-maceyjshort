553cd489abff9cd374d5b2a1d2fb7739b2bf6670
Manav Misra <manavm@visionwebsoft.com>
2020-10-26 14:52:13 -0500

    :bulb: Add 'TODO'

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const form = document.querySelector("form");
 form.addEventListener("submit", function () {
   event.preventDefault();
 
+  // TODO{macey.short}: Collect the form submission into an Object and log it
   console.log(event);
 });
 
