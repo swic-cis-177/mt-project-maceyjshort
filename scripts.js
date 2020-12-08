6f60b45300171af72e114eacb55afe2f63b5cb5b
Manav Misra <manavm@visionwebsoft.com>
2020-12-08 10:14:48 -0600

    :bulb: Add TODO{macey.short}s

--- scripts.js
+++ scripts.js
@@ -3,5 +3,6 @@ import { createTask } from "./lib.js";
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
 
-  console.log(createTask(event.target.elements));
+  // TODO{macey.short}: 'import' another fxn. that will 'render' a 'new task' using the 'template row.
+  createTask(event.target);
 });
