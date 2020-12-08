0e1ef2f0ea35d30eff650b5f3e551aac3992a4ee
Manav Misra <manavm@visionwebsoft.com>
2020-12-08 10:17:27 -0600

    Rewrite 'form processor' to manage checkboxes also.
    Including code to return a new task.
    Including 'TODO'

--- lib.js
+++ lib.js
@@ -3,22 +3,18 @@
  * @param {HTMLFormControlsCollections} elements
  * @returns {Object}
  */
-export const createTask = (elements) =>
-  // Filter out all elements that don't have ids
-  Array.from(elements)
-    .filter(({ id }) => id)
-    .reduce(
-      (formInfo, { id, value }) => ({
-        ...formInfo,
-        [id]: value,
-      }),
-      // Initialize 'formInfo' as an empty object
-      /* const newTaskRow = template.content.cloneNode(true);
+export const createTask = (form) =>
+  // https://dev.to/codefinity/handling-form-submissions-ii-35d5
+  Object.fromEntries(new FormData(form));
+
+export const renderTask = (task) => {
+  // TODO{macey.short}: Use some of this code and the HTML templates to render the task.
+  // Initialize 'formInfo' as an empty object
+  /* const newTaskRow = template.content.cloneNode(true);
       const newTaskTDs = newTaskRow.querySelectorAll("td");
 
       newTaskTDs[0].arrayContent = id.class;
       newTaskTDs[1].arrayContent = id.dueDate;
       newTaskTDs[2].arrayContent = id.goalDueDate;
       newTaskTDs[3].arrayContent = id.task;*/
-      {}
-    );
+};
